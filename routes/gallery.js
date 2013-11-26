1220fa7712fb179988207e1a14afe322b480ad5d
jianping.xwh <jianping.xwh@taobao.com>
2013-11-26 10:03:45 +0800

    修改token

--- routes/gallery.js
+++ routes/gallery.js
@@ -15,7 +15,7 @@ var github = new GitHubApi({
 
 github.authenticate({
 	type: "oauth",
-	token: "7d9e8064e9b3e5d5311c6eabe9fcf6d1243481f8"
+	token: "fa54fd27365f90a9932285323805a6b59568f474"
 });
 
 function renderMD(urlPath, postTitle, res) {
